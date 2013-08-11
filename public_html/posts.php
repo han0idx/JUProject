b230d456720a96a9a58319edd78c439601c8a20b
han0idx <han0idx@gmail.com>
2013-08-11 13:19:50 +0200

    first commit

---
+++ public_html/posts.php
@@ -0,0 +1,23 @@
+<?php
+
+	require_once(realpath(dirname(__FILE__) . "/../resources/config.php"));
+
+	require_once(LIBRARY_PATH . "/templateFunctions.php");
+
+	/*
+		Now you can handle all your php logic outside of the template
+		file which makes for very clean code!
+	*/
+	
+	$setInIndexDotPhp = "Hey! I was set in the index.php file.";
+	$setInIndexDotPhp2 = "WTF";
+	
+	// Must pass in variables (as an array) to use in template
+	$variables = array(
+		'setInIndexDotPhp' => $setInIndexDotPhp,
+		'setInIndexDotPhp2' => $setInIndexDotPhp2
+	);
+	
+	renderLayoutWithContentFile("home.php", $variables);
+
+?>
---
+++ resources/templates/home.php
@@ -0,0 +1,15 @@
+<!-- Homepage content -->
+<h2>Home Page</h2>
+
+<?php
+
+	/*
+		Any variables passed in through the variables parameter in our renderLayoutWithContentPage() function
+		are available in here.
+	*/
+	
+	echo $setInIndexDotPhp;
+	echo "<br>";
+	echo $setInIndexDotPhp2;
+
+?>
